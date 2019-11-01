a908d38058e10041b8f89b9fff8295acdea0d414
wato18 <44283410+wato18@users.noreply.github.com>
2019-11-01 13:07:40 +0900

    update

--- AtC_Beg_Con_011-020/ABC011/C.py
+++ AtC_Beg_Con_011-020/ABC011/C.py
@@ -1,1 +1,16 @@
+from abc import ABCMeta, abstractmethod
 
+class Animal(metaclass=ABCMeta):
+    @abstractmethod
+    def sound(self):
+        print("Hello")
+
+# 抽象クラスを継承
+class Cat(Animal):
+    def sound(self):
+        # 継承元のsoundを呼び出す
+        super(Cat, self).sound()
+        print("Meow")
+
+if __name__ == "__main__":
+    Cat().sound()
