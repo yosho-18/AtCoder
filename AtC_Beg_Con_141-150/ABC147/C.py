d9874192c07ca2119aa46a9b358d9959773b1534
wato18 <44283410+wato18@users.noreply.github.com>
2019-12-12 21:37:10 +0900

    update

--- AtC_Beg_Con_141-150/ABC147/C.py
+++ AtC_Beg_Con_141-150/ABC147/C.py
@@ -2,7 +2,7 @@ import sys
 
 import math, string, itertools, fractions, heapq, collections, re, array, bisect, copy, functools, random
 from collections import deque, defaultdict, Counter; from heapq import heappush, heappop
-# from itertools import permutations, combinations, product, accumulate, groupby
+from itertools import permutations, combinations, product, accumulate, groupby
 # from bisect import bisect_left, bisect_right, insort_left, insort_right
 # from operator import itemgetter as ig
 # sys.setrecursionlimit(10 ** 7)
@@ -31,21 +31,58 @@ def main():
     n = si()
     ixy = [[] for _ in range(n)]
     for i in range(n):
-        for a in range(input()):
-            ixy[i].append(wi())
+        for a in range(int(input())):
+            ixy[i].append([int(m) - 1 for m in input().split()])
 
+    # 真：0，不親切：-1
     nums = [i for i in range(n)]
-    state = [0 for _ in range(n)]
-    queue = deque()
-    for balls in itertools.combinations(nums, 2):
-        balls = list(balls)
-        for x, y in ixy[]:
-            if y == 0:
-                if x - 1 not in balls:
-                    balls.append(x - 1)
-            else:
-                if x - 1
 
+    flag = 0  # 全体の枠組みを抜けるためのflag
+    ans = 0
+    while ans < n and flag == 0:
+        for balls in combinations(nums, ans + 1):
+            balls = list(balls)  # 合計人数を調べるために使用，真だと仮定
+
+            # 初期化する場所に注意する！！
+            queue = deque()
+            hash = defaultdict(lambda: -2)
+
+            for h in balls:
+                hash[h] = 0
+                queue.append(h)
+
+            sign = queue.popleft()
+            brefl = 0  # そのballsでの探索を抜けるためのflag
+            while brefl == 0:
+                for x, y in ixy[sign]:
+                    if y == -1:  # 不親切
+                        if x in hash and hash[x] == 0:  # 0-> -1 ×
+                            flag = 1
+                            brefl = 1
+                            break  # 矛盾
+                        else:  # -2-> -1 ○
+                            hash[x] = -1
+                    else:  # 真
+                        if x not in hash:  # -2-> 0 ○
+                            hash[x] = 0
+                            queue.append(x)
+                            balls.append(x)
+                        elif x in hash and hash[x] == -1:  # -1-> 0 ×
+                            flag = 1
+                            brefl = 1
+                            break  # 矛盾
+
+                if queue == deque():
+                    break  # 探索終了
+                else:
+                    sign = queue.popleft()
+
+
+            if len(balls) > ans and brefl == 0:
+                ans = len(balls)
+                flag = 0
+                break
+    print(ans)
 
 
 if __name__ == '__main__':
